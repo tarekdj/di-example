49a80fbf9263b547d4299c360be48a672f558c1e
Tarek Djebali <tarekdj@gmail.com>
2020-05-28 13:14:19 +0200

    use DI

--- Mailer.php
+++ Mailer.php
@@ -1,6 +1,8 @@
 <?php
 
-class Mailer {
+require 'MailerInterface.php';
+
+class Mailer implements MailerInterface {
   public function send($email, $message) {
     echo "Message '$message' has been sent to '$email' \n";
     sleep(1);
